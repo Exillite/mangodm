3e3c1dc7072c44066fb6a42c461924fef40a1c4b
Exillite <greencod67@gmail.com>
2023-11-25 21:35:55 +0300

    finish v0.1 version

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # MANGO -  simple async ODM for mongodb.
+
+
+### Subdocument save like this:
+```
+"document": {
+    "subdocument": {
+        "_type": "subdocument",
+        "_collection": "{name}",
+        "_id": "{id}"
+    }
+}
+```
+
+### Embedded document save like this:
+```
+"document": {
+    "embedded_document": {
+        "_type": "embedded_document",
+        "_embedded_document": "{embedded_document_name}",
+        // ... other data ...
+    }
+}
--- mangodm.py
+++ mangodm.py
@@ -7,9 +7,6 @@ from typing import Optional, List, Dict, Any, Type, TypeVar
 import logging
 
 
-MONGODB_URL = ""
-MONGODB = ""
-
 logger = logging.getLogger('mangodm_logger')
 logging.basicConfig(filename='mangodm.log',
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -19,35 +16,48 @@ class DataBase:
     client: AsyncIOMotorClient  # type: ignore
     db: AsyncIOMotorDatabase  # type: ignore
 
-    def __init__(self, connect_url, database_name) -> None:
-        self.client = AsyncIOMotorClient(connect_url)
-        self.db = self.client[database_name]
 
-    async def connect_to_mongo(self):
-        db.client = AsyncIOMotorClient(f"{MONGODB_URL}")
-        db.db = db.client[MONGODB]
+db: DataBase = DataBase()
 
-    def close_connection(self) -> None:
-        self.client.close()
+"""
+COLLECTIONS["{collection_name}"] = {Model}
+"""
+COLLECTIONS: Dict[str, Type["Document"]] = {}
+EMBEDDED_COLLECTIONS: Dict[str, Type["EmbeddedDocument"]] = {}
 
 
-db: DataBase
+async def connect_to_mongo(MONGODB_URL: str, MONGODB: str):
+    db.client = AsyncIOMotorClient(f"{MONGODB_URL}")
+    db.db = db.client[MONGODB]
+
+
+def close_connection():
+    db.client.close()
+
 
 T = TypeVar('T', bound='Document')
 EmbdT = TypeVar('EmbdT', bound='EmbeddedDocument')
 
 
 class EmbeddedDocument(BaseModel):
+    @classmethod
+    def register_embedded_collection(cls):
+        EMBEDDED_COLLECTIONS[cls.__name__] = cls
+
     def to_document(self) -> Dict[str, Any]:
-        model_dict: Dict[str, Any] = self.model_dump()
-        document: Dict[str, Any] = {}
+        model_dict: Dict[str, Any] = dict(self)
+        document: Dict[str, Any] = {
+            "_type": "embedded_document",
+            "_embedded_document": self.__class__.__name__
+        }
 
         for key, value in model_dict.items():
+            print("!!!", value)
             if isinstance(value, Document):
                 if not value.is_saved():
                     logger.error("Subdocument not saved")
                     raise Exception("Subdocument not saved")
-                document[key] = value.id
+                document[key] = value.to_subdocument()
             elif isinstance(value, EmbeddedDocument):
                 document[key] = value.to_document()
             else:
@@ -56,7 +66,7 @@ class EmbeddedDocument(BaseModel):
         return document
 
     def to_response(self) -> Dict[str, Any]:
-        model_dict: Dict[str, Any] = self.model_dump()
+        model_dict: Dict[str, Any] = dict(self)
         response: Dict[str, Any] = {}
 
         for key, value in model_dict.items():
@@ -70,14 +80,33 @@ class EmbeddedDocument(BaseModel):
         return response
 
     @classmethod
-    def document_to_model(cls: Type[EmbdT], document: Dict[str, Any]) -> EmbdT:
+    async def document_to_model(cls: Type[EmbdT], document: Dict[str, Any]) -> EmbdT:
         for key, value in document.items():
-            if isinstance(value, ObjectId):
-                document[key] = Document.get(id=value)
-            elif isinstance(value, dict):
-                document[key] = cls.document_to_model(value)
+            if isinstance(value, dict):
+                if value['_type'] == 'subdocument':
+                    document[key] = await Document.from_subdocument(value)
+                elif value['_type'] == 'embedded_document':
+                    document[key] = await EmbeddedDocument.from_subembedded(value)
 
         return cls(**document)
+
+    @classmethod
+    async def from_subembedded(cls, subdocument: Dict[str, Any]):
+        if subdocument['_type'] != 'embedded_document':
+            logger.error("Invalid subdocument")
+            raise Exception("Invalid subdocument")
+        if not subdocument['_embedded_document'] in EMBEDDED_COLLECTIONS:
+            logger.error("Invalid subdocument")
+            raise Exception("Invalid subdocument")
+
+        del subdocument['_type']
+
+        embedded_document_name = subdocument['_embedded_document']
+        del subdocument['_embedded_document']
+
+        submodel = await EMBEDDED_COLLECTIONS[embedded_document_name] \
+            .document_to_model(subdocument)
+        return submodel
 
 
 class Document(BaseModel):
@@ -89,39 +118,39 @@ class Document(BaseModel):
         excludeFieldsResponse: List[str] = []
 
     @classmethod
-    def get_fields(cls) -> Dict[str, Any]:
-        properties = cls.model_json_schema().get("properties")
-        if properties:
-            return properties
-        else:
-            logging.error("No fields found")
-            raise Exception("No fields found")
+    def register_collection(cls):
+        if not 'collection_name' in cls.Config.__dict__:
+            cls.Config.collection_name = cls.__name__
+        COLLECTIONS[cls.Config.collection_name] = cls
 
     def is_saved(self) -> bool:
         return self.id != "-1"
 
-    def to_document(self) -> Dict[str, Any]:
-        if not self.is_saved():
-            logger.error("Document not saved")
-            raise Exception("Document not saved")
+    def to_document(self, for_create=False) -> Dict[str, Any]:
+        if for_create:
+            if self.is_saved():
+                logger.error("Document already created")
+                raise Exception("Document already created")
+        else:
+            if not self.is_saved():
+                logger.error("Document not saved")
+                raise Exception("Document not saved")
 
-        model_dict: Dict[str, Any] = self.model_dump()
-        document: Dict[str, Any] = {
-            '_id': ObjectId(model_dict['id'])
-        }
+        model_dict: Dict[str, Any] = dict(self)
+        document: Dict[str, Any] = dict()
+        if not for_create:
+            document['_id'] = ObjectId(model_dict['id'])
         del model_dict['id']
 
         if 'excludeFields' in self.Config.__dict__:
             for field in self.Config.excludeFields:
                 del model_dict[field]
-
         for key, value in model_dict.items():
             if isinstance(value, Document):
                 if not value.is_saved():
                     logger.error("Subdocument not saved")
                     raise Exception("Subdocument not saved")
-                document[key] = value.id
-
+                document[key] = value.to_subdocument()
             elif isinstance(value, EmbeddedDocument):
                 document[key] = value.to_document()
             else:
@@ -134,7 +163,7 @@ class Document(BaseModel):
             logger.error("Document not saved")
             raise Exception("Document not saved")
 
-        model_dict: Dict[str, Any] = self.model_dump()
+        model_dict: Dict[str, Any] = dict(self)
         response: Dict[str, Any] = {}
 
         if 'excludeFieldsResponse' in self.Config.__dict__:
@@ -151,16 +180,39 @@ class Document(BaseModel):
 
         return response
 
+    def to_subdocument(self) -> Dict[str, Any]:
+        if not self.is_saved():
+            logger.error("Document not saved")
+            raise Exception("Document not saved")
+
+        document: Dict[str, Any] = {
+            "_type": "subdocument",
+            "_collection": self.Config.collection_name,
+            "_id": self.id
+        }
+
+        return document
+
     @classmethod
-    def document_to_model(cls: Type[T], document: Dict[str, Any]) -> T:
+    async def from_subdocument(cls, subdocument: Dict[str, Any]):
+        if subdocument["_type"] != "subdocument":
+            logger.error("Invalid subdocument")
+            raise Exception("Invalid subdocument")
+        model = await COLLECTIONS[subdocument['_collection']].get(
+            id=subdocument["_id"])
+
+        return model
+
+    @classmethod
+    async def document_to_model(cls: Type[T], document: Dict[str, Any]) -> T:
+        document['id'] = str(document['_id'])
+        del document['_id']
         for key, value in document.items():
-            if key == '_id':
-                document['id'] = str(value)
-                del document['_id']
-            if isinstance(value, ObjectId):
-                document[key] = Document.get(id=value)
-            elif isinstance(value, dict):
-                document[key] = cls.document_to_model(value)
+            if isinstance(value, dict):
+                if value['_type'] == 'subdocument':
+                    document[key] = await Document.from_subdocument(value)
+                elif value['_type'] == 'embedded_document':
+                    document[key] = await EmbeddedDocument.from_subembedded(value)
 
         return cls(**document)
 
@@ -172,7 +224,7 @@ class Document(BaseModel):
 
         document = await db.db[cls.Config.collection_name].find_one(kwargs)
         if document:
-            return cls.document_to_model(document)
+            return await cls.document_to_model(document)
         return None
 
     @classmethod
@@ -184,20 +236,25 @@ class Document(BaseModel):
         cursor = db.db[cls.Config.collection_name].find(kwargs)
         models: List[T] = []
         async for document in cursor:
-            models.append(cls.document_to_model(document))
+            models.append(await cls.document_to_model(document))
 
         return models
 
     async def create(self):
-        new_document = self.to_document()
+        new_document = self.to_document(for_create=True)
         result = await db.db[self.Config.collection_name].insert_one(new_document)
         self.id = str(result.inserted_id)
 
-    async def update(self):
+    async def update(self, recursive_update=False):
         document = self.to_document()
         if not self.is_saved():
             logger.error("Document not saved")
             raise Exception("Document not saved")
+        if recursive_update:
+            for value in dict(self).values():
+                if isinstance(value, Document):
+                    await value.update(recursive_update=True)
+
         await db.db[self.Config.collection_name].update_one(
             {'_id': ObjectId(self.id)}, {'$set': document})
 
